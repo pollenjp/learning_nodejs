b69c2eaf948cb42f7e6ab027970cea624e6a37b6
pollenjp <polleninjp@gmail.com>
2018-05-16 03:40:23 +0900

    [Update] nodejs_beginners_book/01_basic_http_server/server.js

--- nodejs_beginners_book/01_basic_http_server/server.js
+++ nodejs_beginners_book/01_basic_http_server/server.js
@@ -7,14 +7,15 @@ var http = require("http");     // 変数httpを用いてNode.jsに同梱され�
 
 function onRequest(request, response)
 {
+    console.log("Request received.");
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write("Hello World!");
     response.end();
 }
 
-
 http.createServer(onRequest).listen(8888);      // callback -> onRequest
 
+console.log("Server has started.");
 
 
 
