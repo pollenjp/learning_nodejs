c8e64f76412061f9cd6e7f0651ea86f15a6e3e47
pollenjp <polleninjp@gmail.com>
2018-05-16 01:58:15 +0900

    [Update] callback - server.js

--- nodejs_beginners_book/01_basic_http_server/server.js
+++ nodejs_beginners_book/01_basic_http_server/server.js
@@ -5,14 +5,15 @@
 var http = require("http");     // 変数httpを用いてNode.jsに同梱されているhttpモジュールへのアクセスを可能にする
 
 
-http.createServer(
-    function(request, response)
-    {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World!");
-        response.end();
-    }
-).listen(8888);
+function onRequest(request, response)
+{
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World!");
+    response.end();
+}
+
+
+http.createServer(onRequest).listen(8888);      // callback -> onRequest
 
 
 
