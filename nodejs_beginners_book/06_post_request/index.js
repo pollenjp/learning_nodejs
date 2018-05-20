55bd833974636bb49e5aca5b1c160c2a0680801f
pollenjp <polleninjp@gmail.com>
2018-05-20 16:37:06 +0900

    [Add] 06_post_request/

---
+++ nodejs_beginners_book/06_post_request/index.js
@@ -0,0 +1,21 @@
+// index.js
+
+var server = require("./server");       // server.js module
+var router = require("./router");       // router.js module
+var requestHandlers = require("./requestHandlers");
+
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+
+//--------------------
+//  server start
+//--------------------
+server.start(router.route, handle);
+
+
+
+
---
+++ nodejs_beginners_book/06_post_request/requestHandlers.js
@@ -0,0 +1,48 @@
+// requestHandlers.js
+
+
+//--------------------
+//  start()
+//  upload()
+//--------------------
+function start(response, postData)
+{
+    console.log("Request handler 'start' was called.");
+
+
+    var body = '<html>'+
+        '<head>'+
+        '<meta http-equiv="Content-Type" content="text/html; '+
+        'charset=UTF-8" />'+
+        '</head>'+
+        '<body>'+
+        '<form action="/upload" method="post">'+
+        '<textarea name="text" rows="20" cols="60"></textarea>'+
+        '<input type="submit" value="Submit text" />'+
+        '</form>'+
+        '</body>'+
+        '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function upload(response, postData)
+{
+    console.log("Request handler 'upload' was called.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    //response.write("Hello World!");
+    response.write("You've sent: " + postData);
+    response.end();
+}
+
+
+//--------------------
+//  exports
+//--------------------
+exports.start = start;
+exports.upload = upload;
+
+
+
