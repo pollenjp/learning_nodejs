f158a33b6a7ba2d7b1ae8b41bb446a049f3fff4d
pollenjp <polleninjp@gmail.com>
2018-06-11 05:50:56 +0900

    [Add] nodejs_chonyuumon/ch02/3/

---
+++ nodejs_chonyuumon/ch02/3/app.js
@@ -0,0 +1,30 @@
+//  index.js
+
+
+const http = require("http");
+const fs = require("fs");
+const ejs = require("ejs");
+
+
+//----------------------------------------
+//  main
+//----------------------------------------
+var index_page = fs.readFileSync("./index.ejs", "utf8");
+
+var server = http.createServer(getFromClient);
+
+server.listen(3000);
+console.log("Server start!");
+
+
+//----------------------------------------
+//  createServer
+//----------------------------------------
+function getFromClient(req, res)
+{
+  var content = ejs.render(index_page);
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.write(content);
+  res.end();
+}
+
