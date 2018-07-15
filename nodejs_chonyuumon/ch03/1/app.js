d627bc439e54621210b7172360ca0289d036f60a
pollenjp <polleninjp@gmail.com>
2018-07-16 08:12:30 +0900

    [Update] Chapter03 - Section 2 - Display another data / nodejs_chonyuumon/ch03/1/

--- nodejs_chonyuumon/ch03/1/app.js
+++ nodejs_chonyuumon/ch03/1/app.js
@@ -78,6 +78,13 @@ function response_index(req, res)
     'Ichiro': '060-666-666'
   };
 
+  var data2 = {
+    'Taro': ['taro@yamada', '09-999-999', 'Tokyo'],
+    'Hanako': ['hanako@flower', '080-888-888', 'Yokohama'],
+    'Sachiko': ['sachi@happy', '080-888-888', 'Nagoya'],
+    'Ichiro': ['Ichiro', '060-666-666', 'USA']
+  }
+
   var msg = "これはIndexページです."
   // Rendering a HTML page
   var content = ejs.render( index_page,
@@ -99,61 +106,27 @@ function response_index(req, res)
 //--------------------------------------------------------------------------------
 function response_other(req, res)
 {
-  var msg = "これはOtherページです."
-  if ( req.method == 'POST' ){
-    // Post Access Processing
-    var body = '';
-
-    // data Event
-    req.on('data', (data) => {
-      // ref: https://nodejs.org/api/events.html#events_emitter_on_eventname_listener
-      console.log("request.on(1)");
-      body += data;  // get post data
-    });
-
-    // end Event
-    req.on('end', () => {
-      console.log("request.on(2)");
-      var post_data = qs.parse(body);  // parse post data
-
-      //----------
-      // Check Log
-      //----------
-      // "msg=aaa+bbb", if you fill "aaa bbb" in the form.
-      console.log(body);
-      console.log(post_data);  // "msg=aaa+bbb", if you fill "aaa bbb" in the form.
-      //----------
-
-      // "msg=aaa+bbb", if you fill "aaa bbb" in the form.
-      msg += "あなたは,「" + post_data.msg + "」と書きました。";
-      // Rendering a HTML page
-      var content = ejs.render(
-        other_page,
-        {
-          title: "Other",
-          content: msg
-        }
-      );
-      res.writeHead( 200, {'Content-Type': 'text/html'} );
-      res.write(content);
-      res.end();
-    });
-  } else {
-    // Get Access Processing
-    var msg = "ページがありません.";
-    // Rendering a HTML page
-    var content = ejs.render(
-      other_page,
-      {
-        title: "Other",
-        content: msg
-      }
-    );
-    // Response Processing
-    res.writeHead( 200, {"Content-Type": 'text/html'} );
-    res.write(content);
-    res.end();
+  var data2 = {
+    'Taro'   : ['taro@yamada',   '09-999-999',  'Tokyo'],
+    'Hanako' : ['hanako@flower', '080-888-888', 'Yokohama'],
+    'Sachiko': ['sachi@happy',   '080-888-888', 'Nagoya'],
+    'Ichiro' : ['Ichiro',        '060-666-666', 'USA'],
   }
+
+  var msg = "これはOtherページです."
+
+  // Rendering a HTML page
+  var content = ejs.render( other_page,
+                            {
+                              title: "Other",
+                              content: msg,
+                              data: data2,
+                              filename: 'data_item',
+                            } );
+  // Response Processing
+  res.writeHead( 200, {"Content-Type": 'text/html'} );
+  res.write(content);
+  res.end();
 }
 
 
